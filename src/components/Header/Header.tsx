44ea8f4c0498ae152570e5f6387f42bed7d5d97f
benjamin <benjamin.choron@outlook.com>
2023-08-17 17:32:40 +0200

    feat(header): add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+
+export default function Header() {
+  const [search, setSearch] = React.useState("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(search);
+    setSearch("");
+  };
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.header__title}>🎥 My Movies</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={styles.header__searchInput}
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button type="submit" className={styles.header__searchButton}>
+          Search
+        </button>
+      </form>
+    </header>
+  );
+}
